c85eb081f2aa5d667af2158525c26505cd3e6426
NayaRPL <naya.mmj@gmail.com>
2021-12-29 20:25:17 +0800

    tuple

---
+++ day 18 programing.py
@@ -0,0 +1,10 @@
+print("tipe data tuple")
+nama=10,4,8,28,"naya" #didalam data tuple itu bisa tidak mengggunakan dalam kurung
+print(nama)
+nama=(10,6,"naya","apa",False,96)
+print(nama)
+N=(10,6,"naya","apa",False,96)
+N.index(100)
+print(N)
+
+
